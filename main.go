4173c826889c0c1bbd76b7296c8c3b09b2125130
samar <scipioresearch2020@gmail.com>
2026-02-21 10:25:27 +0530

    sorting

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/akamensky/argparse"
 )
@@ -44,22 +45,27 @@ func main() {
 	}
 }
 
+type SizeEntry struct {
+	Name string
+	Size float64
+}
+
 type DirSizeBreakdown struct {
-	DirSizes  map[string]float64
-	FileSizes map[string]float64
+	DirSizes  []SizeEntry
+	FileSizes []SizeEntry
 }
 
 func (d DirSizeBreakdown) String() string {
 	var out string
 	out += "Directory Sizes:\n"
 	out += "----------------\n"
-	for name, size := range d.DirSizes {
-		out += fmt.Sprintf("%-30s: %.2f Mb\n", name, size)
+	for _, entry := range d.DirSizes {
+		out += fmt.Sprintf("%-30s: %.2f Mb\n", entry.Name, entry.Size)
 	}
 	out += "\nFile Sizes:\n"
 	out += "-----------\n"
-	for name, size := range d.FileSizes {
-		out += fmt.Sprintf("%-30s: %.2f Mb\n", name, size)
+	for _, entry := range d.FileSizes {
+		out += fmt.Sprintf("%-30s: %.2f Mb\n", entry.Name, entry.Size)
 	}
 	return out
 }
@@ -91,42 +97,50 @@ func GetDirSizeBreakdown(folder string) (DirSizeBreakdown, error) {
 
 	}
 
-	breakdown := DirSizeBreakdown{
-		DirSizes:  make(map[string]float64),
-		FileSizes: make(map[string]float64),
-	}
+	breakdown := DirSizeBreakdown{}
+	totalSize := 0.0
 
 	for _, file := range files {
-
 		if file.Name()[0] == '.' {
-
 			continue
-
 		}
 
 		fullPath := filepath.Join(folder, file.Name())
 
 		if file.IsDir() {
-
 			size, err := GetRecursiveDirSize(fullPath)
-
 			if err != nil {
-
 				continue
-
 			}
-
-			breakdown.DirSizes[file.Name()] = float64(size) / 1024 / 1024
-
+			breakdown.DirSizes = append(breakdown.DirSizes, SizeEntry{
+				Name: file.Name(),
+				Size: float64(size) / 1024 / 1024,
+			})
+			totalSize += float64(size) / 1024 / 1024
 		} else {
-
 			info, err := file.Info()
 			if err != nil {
 				continue
 			}
-			breakdown.FileSizes[file.Name()] = float64(info.Size()) / 1024 / 1024
+			breakdown.FileSizes = append(breakdown.FileSizes, SizeEntry{
+				Name: file.Name(),
+				Size: float64(info.Size()) / 1024 / 1024,
+			})
+			totalSize += float64(info.Size()) / 1024 / 1024
 		}
 	}
 
+	sort.Slice(breakdown.DirSizes, func(i, j int) bool {
+		return breakdown.DirSizes[i].Size > breakdown.DirSizes[j].Size
+	})
+
+	sort.Slice(breakdown.FileSizes, func(i, j int) bool {
+		return breakdown.FileSizes[i].Size > breakdown.FileSizes[j].Size
+	})
+
+	breakdown.DirSizes = append(breakdown.DirSizes, SizeEntry{
+		Name: "Total",
+		Size: totalSize,
+	})
 	return breakdown, nil
 }
